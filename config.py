af6d933932248b39f4951d542e82a3c471cc7447
anonym1479 <112720523+anonym1479@users.noreply.github.com>
2026-07-28 20:27:55 +0200

    chore: remove legacy v1 files from v2 branch

--- config.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("config.json", "r", encoding="utf-8") as file:
-    config = json.load(file)
-
-# List of all channels for TwitchIO to join
-CHANNELS = config.get("channels", [])
-CHANNEL_USERNAMES = [ch["username"] for ch in CHANNELS]
-CHANNEL_MAP = {ch["username"]: ch["user_id"] for ch in CHANNELS}
